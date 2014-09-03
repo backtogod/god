ce72cc3382af623640b716c2ec1fec16c54128b5
yestein <yestein>
2014-09-03 22:16:23 +0800

    Robot to be continued

--- src/god/model/ai/robot.lua
+++ src/god/model/ai/robot.lua
@@ -9,8 +9,7 @@ if not Robot then
 	Robot = ModuleMgr:NewModule("robot")
 end
 
-Robot:DeclareListenEvent("GAME.ACTION_START", "OnActionStart")
-Robot:DeclareListenEvent("GAME.END_WATCH", "OnEndWatch")
+Robot:DeclareListenEvent("GAME_STATE.CHANGE", "OnGameState")
 
 function Robot:_Uninit()
 	return 1
@@ -20,10 +19,22 @@ function Robot:_Init()
 	return 1
 end
 
-function Robot:OnActionStart(round)
-	-- body
-end
+function Robot:OnGameState(state)
+	if state ~= GameStateMachine.STATE_ENEMY_OPERATE then
+		return
+	end
+	local can_move_list = {}
+	local can_pick_list = {}
+	for logic_x = 1, Def.MAP_WIDTH do
+		if EnemyMap:GetCell(logic_x, 1) >= 0 then
+			can_pick_list[#can_pick_list + 1] = logic_x
+		end
+		if EnemyMap:GetCell(logic_x, Def.MAP_HEIGHT) <= 0 then
+			can_move_list[#can_move_list + 1] = logic_x
+		end
+	end
 
-function Robot:OnEndWatch()
-	-- body
+	local pick_x = math.random(1, #can_pick_list)
+	print("Robot pick", pick_x)
+	-- local ret_code, result = CommandCenter:ReceiveCommand({"PickChess", pick_x})
 end

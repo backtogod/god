c194ef04e0b71c5273170b2a47418f004c341d76
yestein <yestein>
2014-09-27 23:56:19 +0800

    Modify Random Spawn to avoid "Lucky"

--- src/god/model/module/chess_spawner.lua
+++ src/god/model/module/chess_spawner.lua
@@ -19,11 +19,45 @@ function ChessSpawner:_Init( ... )
 end
 
 function ChessSpawner:SpawnChess(map, id_list)
+	local last_logic_y = nil
+	local last_generate_id = nil
 	for logic_x = 1, Def.MAP_WIDTH do
 		local logic_y = Mover:GetMoveablePosition(map, logic_x)
+		local generate_id = nil
 		if logic_y > 0 then
-			local id = id_list and id_list[logic_x] or math.random(1, 6)
-			map.obj_pool:Add(Chess, id, logic_x, logic_y)
+			if id_list and id_list[logic_x] then
+				generate_id = id_list[logic_x]
+			else
+				local except_list = {}
+				if last_generate_id and logic_y == last_logic_y then
+					except_list[last_generate_id] = 1
+				end
+				
+				local id_top = map:GetTopCell(logic_x)
+				local id_second = map:GetTopCell(logic_x, 2)
+				if id_top and id_second then
+					local chess_top = map.obj_pool:GetById(id_top)
+					local chess_second = map.obj_pool:GetById(id_second)
+					local state_top = chess_top:TryCall("GetState")
+					local state_second = chess_second:TryCall("GetState")
+
+					local template_top = chess_top:GetTemplateId()
+					local template_second = chess_second:GetTemplateId()
+					if state_top == Def.STATE_NORMAL and state_second == Def.STATE_NORMAL and template_top == template_second then
+						except_list[template_top] = 1
+					end
+				end
+				local generate_list = {}
+				for i = 1, 6 do
+					if not except_list[i] then
+						generate_list[#generate_list + 1] = i
+					end
+				end
+				generate_id = generate_list[math.random(1, #generate_list)]
+			end
+			map.obj_pool:Add(Chess, generate_id, logic_x, logic_y)
 		end
+		last_logic_y = logic_y
+		last_generate_id = generate_id
 	end
 end
